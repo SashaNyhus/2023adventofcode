4adc70703eb6e160b36c9fad06541a08735dd7cc
Sasha Nyhus <sashanyhus@icloud.com>
2023-12-04 12:00:46 -0800

    helper functions and day 2 input

---
+++ day02.py
@@ -0,0 +1,3 @@
+from helpers import get_input_as_dict
+
+input_dict = get_input_as_dict('02')
---
+++ helpers.py
@@ -0,0 +1,15 @@
+def get_input_as_list(day_key):
+    new_input = []
+    with open(f'inputs/day{day_key}.txt', 'r') as file:
+        for line in file:
+            new_input.append(line.strip('\n'))
+    file.close()
+    return new_input
+
+def get_input_as_dict(day_key):
+    new_dict = {}
+    list_input = get_input_as_list(day_key)
+    for line in list_input:
+        split_line = line.split(":")
+        new_dict[split_line[0]] = split_line[1]
+    return
