e55095f6aa5d0c68d51a5ae96620f4443177b3b3
Sasha Nyhus <sashanyhus@icloud.com>
2023-12-07 20:18:11 -0800

    day 02 part 1

--- day02.py
+++ day02.py
@@ -1,9 +1,11 @@
+from cProfile import run
 from helpers import get_input_as_dict
 
 input_dict = get_input_as_dict('02')
 print(input_dict)
 cleaned_input = {}
 for game_key, game_data in input_dict.items():
+    game_key_int = game_key.split(' ')[1]
     split_games = game_data.split(';')
     reformatted_draws = []
     for draw in split_games:
@@ -16,5 +18,26 @@ for game_key, game_data in input_dict.items():
             print(draw_data)
         reformatted_draws.append(draw_data)
         # print(draw)
-    cleaned_input[game_key] = reformatted_draws
+    cleaned_input[game_key_int] = reformatted_draws
 print(cleaned_input)
+
+color_totals = {
+    'red': 12,
+    'green': 13,
+    'blue': 14
+}
+
+running_total = 0
+for game_key, game_data in cleaned_input.items():
+    valid_game = True
+    for draw in game_data:
+        for color, amount in draw.items():
+            if color_totals[color] < int(amount):
+                valid_game = False
+                print(f'Game {game_key} is invalid because of {color}')
+                break
+        if valid_game == False:
+            break
+    if valid_game:
+        running_total = running_total + int(game_key)
+print(running_total)
