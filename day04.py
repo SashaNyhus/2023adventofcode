a5c0f3048a616f39c10c1c7d14bda8b6c3e77323
Sasha Nyhus <sashanyhus@icloud.com>
2023-12-11 22:47:20 -0800

    day04 part 2

--- day04.py
+++ day04.py
@@ -22,4 +22,36 @@ def get_part_1_answer(input_dict):
         total_score = total_score + card_score
     return total_score
 
-print(get_part_1_answer(input_dict))
+# print(get_part_1_answer(input_dict))
+
+def get_card_matches(card_list):
+    winning_numbers = set(card_list[0].split(" "))
+    winning_numbers.remove('')
+    had_numbers = set(card_list[1].split(" "))
+    had_numbers.remove('')
+    matched_numbers = winning_numbers.intersection(had_numbers)
+    match_count = len(matched_numbers)
+    return match_count
+
+
+def get_part_2_answer(input_dict):
+    total_cards = 0
+    copies_by_card = {i: 1 for i in range(1, (len(input_dict) + 1))}
+    print(copies_by_card)
+    for card_set in input_dict:
+        card_id = int(card_set.strip("Card "))
+        copies_of_this_card = copies_by_card[card_id]
+        total_cards = total_cards + copies_of_this_card
+
+        card_list = input_dict[card_set].split("|")
+        card_matches = get_card_matches(card_list=card_list)
+        print(card_matches)
+        next_card = card_id + 1
+        while card_matches > 0:
+            copies_by_card[next_card] += copies_of_this_card
+            card_matches = card_matches - 1
+            next_card += 1
+        print(copies_by_card)
+    return total_cards
+
+print(get_part_2_answer(input_dict))
